ad45566f4f308fc0a7ab7b6c3e25e16d3b10f996
Andre <marques.andre.lm@gmail.com>
2019-09-25 20:06:36 +0100

    Fixed delete database

--- src/flushdb.js
+++ src/flushdb.js
@@ -7,12 +7,8 @@ mongoose.connect('mongodb://' + process.env.MONGO_DB + '/weather', {
 }, (err) => {
     if (err) throw err;
     console.log('Connection established...');
-    mongoose.connection.db.listCollections().toArray(function (err, names) {
-        names.forEach((model) => {
-            mongoose.connection.db.dropCollection(model, function(err, result) {
-                console.log(model.name + ' deleted');
-            });
-        });
+    mongoose.connection.db.dropDatabase().then(() => {
+        console.log('Database deleted!');
         mongoose.disconnect();
     });
 });
